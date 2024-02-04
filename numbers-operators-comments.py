cac35d187c0bae47dd6c1dfc6f715182ce8410fa
Otuekong Etim <myshield02@gmail.com>
2024-02-04 21:22:53 +0100

    handled operators in python

--- numbers-operators-comments.py
+++ numbers-operators-comments.py
@@ -21,3 +21,42 @@ This is another block level comment
 But this one is using a single quote
 Note that you must be consistent with the type of quotes you are using
 '''
+###################### OPERATORS ###############################
+"""
+# OPERATORS
+---------------------------------------------------------------
+Operators are simply signs that tells the computer what to do.
+There a few categories,
+1. Mathematical operators
+These are simple the mathematical signs that we use in mathematics
+Examples include: 
+    '+': for addition
+    '-': for subtraction
+    '/': for division
+    '*': for multiplication
+    '%': called modulo. It is used to get the remainder of a division operation
+    '//': called integer devision. Used to get only the whole number part of a division operation
+    '**': for raising a number to a power
+
+2. Logical operators
+These are used for comparisions
+Examples include:
+    '>': greater than
+    '<': less than
+    '==': checking for equlity
+    '!=': not equal to
+"""
+print('addition', 2+4) # this is addition
+print('subtraction', 4-4) # this is subtraction
+print('division', 4/2) # this is division
+print('multiplication', 3*2) # this is multiplication
+print('modulo division', 6%4) # this will give only the remainder of the division
+print('integer division', 6//4) # this will give only the whole number part of the division
+print(4%3)
+print('raise to a power', 2**3) # this is raising a number to a power
+
+print(2>4) # this is for greater than
+print(2<4) # this is for less than
+print(2==4) # checking for equality
+print(2!=4) # checking not equality
+
