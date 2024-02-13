de297964f82f491d012cd60575586d29a146a02f
Emem Udofia <emysmyles@gmail.com>
2024-02-13 17:26:27 +0100

    all about comments

--- numbers-operators-comments.py
+++ numbers-operators-comments.py
@@ -1,1 +1,23 @@
--
+print ('This is a print statement') #this is a code to print something to the screen
+print ('This is the first day of programming with python') #another code to print to the screen
+print ('this is part of a comment') # The keyboard shortcut to a single line comment is ctrl + /
+print ('just for practice') 
+
+# this is a single line comment
+# this is another single line comment
+# this is a third single line comment
+"""
+This is for a block level comment
+Block level means multiple lines 
+So with this type of comment i can write a paragraph on multiple lines
+
+It is very common in python to use block level comments for documentation purpose
+Usually this is done when writing a function or a class in python
+It can also be used to document a whole script
+"""
+# Below we use single quotes for another block level comment
+'''
+This is another block level comment
+But this one is using a single quote
+Note that you must be consistent with the type of quotes you are using
+'''
