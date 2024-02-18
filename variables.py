8fff931725a2897c90ab969e67b97e4102b80443
Emem Udofia <emysmyles@gmail.com>
2024-02-18 19:23:03 +0100

    a brief note and some examples on variables

---
+++ variables.py
@@ -0,0 +1,25 @@
+# Examples of varaibles
+name = "Emem"
+print("The value of the var name is", name)
+age = 120
+gender = "female"
+parent = "father" #object oriented programming - class
+old = True
+name = "Itoro"
+print("The value of the var name is",name)
+
+first_number = 50
+next_number = 26
+result = first_number + next_number
+print(result)
+
+
+# we can check the type of value stored in a variable
+print(type(name)) # this will check the data type of the value stored in the variable called name
+print(type(next_number)) # this will check the data type of the value stored in the variable called next_number
+print(type(old)) # this will check the data type of the value stored in the variable called old
+
+print() #the bracket is there because it is a function
+type() #the bracket is there because it is a function
+name # there is no bracket because it is just a variable
+
