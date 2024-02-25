d2d4370ad669b2aeabf110c05c460fe73938f93a
mayen <mayendan20@gmail.com>
2024-02-25 21:47:14 +0100

    file moved

--- first.py
+++
@@ -1,2 +0,0 @@
-print('there is fun in pgython')
-print('i am very happy')
--- my-details.py
+++
@@ -1,7 +0,0 @@
-"""
-Dan, Mayen M. I haven't changed my name officially.
-Akwa Ibom State.
-40
-Brown because it isn't everybody's favourite.
-13 because it's my birth date.
-"""
