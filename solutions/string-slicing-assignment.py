1cdd4dbbcc199587427f21c728207290d36cbd6e
Emem Udofia <emysmyles@gmail.com>
2024-03-09 14:15:46 +0100

    first solution

---
+++ solutions/string-slicing-assignment.py
@@ -0,0 +1,44 @@
+"""
+# Question 1
+-------------------------------------------------------
+Create a variable for the string 'Python is Fun!' and
+write the commands that will return the following
+- 'ytho'
+- 'Pytho'
+- 'ython is Fun!'
+- 'Python is Fun!'
+- 'yhni u'
+- 'yoiF'
+- 'Pto sFn'
+"""
+#Solution to question 1
+Statement = "Python is Fun!"
+print (Statement[1:5])
+print (Statement[0:5])
+print (Statement[1:13])
+print (Statement[0:14])
+print (Statement[1:11:2])
+print (Statement[1:11:3])
+print (Statement[0:13:2])
+
+"""
+# Question 2
+-------------------------------------------------------
+Create 3 variables to store 
+- 'hello'
+- ','
+- 'world'
+and a fourth variable that will be a concatenation of 
+the first and third variable.
+Write a command that will give the following output
+- 'ello'
+- 'elloworl'
+- 'elwr'
+- 'dlrowolleh'
+"""
+
+#Solution 2
+
+
+
+
