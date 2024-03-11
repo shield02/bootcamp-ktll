677758916faf7a2996e7d6cafd002f158c080102
Oba <okunadetewogbade@gmail.com>
2024-03-11 12:00:59 -0700

    solutions/03-march.py

---
+++ solutions/03-march.py
@@ -0,0 +1,44 @@
+"""
+# Question 1
+-------------------------------------------------------
+Create a variable for the string 'Python is Fun!' and
+write the commands that will return the following
+- 'ytho'
+- 'Pytho'
+- 'ython is Fun!'
+- 'Python is Fun!'
+- 'yhni u'
+- 'yoiF'
+- 'Pto sFn'
+"""
+
+"""
+# Question 2
+-------------------------------------------------------
+Create 3 variables to store 
+- 'hello'
+- ','
+- 'world'
+and a fourth variable that will be a concatenation of 
+the first and third variable.
+Write a command that will give the following output
+- 'ello'
+- 'elloworl'
+- 'elwr'
+- 'dlrowolleh'
+"""
+
+# Question 1
+
+learning = ("python is fun!")
+
+
+# Question 2
+
+greeting = ('hello')
+sign = ("','")
+location = ('world')
+quickie = (greeting + location)
+
+'ello' = ('[1],[2],[2],[3]')
+'elloworl' = ('[1],[2],[2],[3]+[0],[1],[2],[3]')
