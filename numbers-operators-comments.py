c8d308211bc62efd9a68bec3384b6f69e2575320
emysmyles <150858124+emysmyles@users.noreply.github.com>
2024-03-13 00:23:18 +0100

    Revert "Emem"

--- numbers-operators-comments.py
+++ numbers-operators-comments.py
@@ -1,15 +1,13 @@
-print ('This is a print statement') #this is a code to print something to the screen
-print ('This is the first day of programming with python') #another code to print to the screen
-print ('this is part of a comment') # The keyboard shortcut to a single line comment is ctrl + /
-print ('just for practice') 
+print('This is a print statement') # this is a code to print something to the screen
+print('It is the first day of programming with python') # another code to print to screen
+print('this is part of a comment') # the keyboard shortcut to comment a single is ctrl + /
+print('just for fun')
 
 # this is a single line comment
 # this is another single line comment
 # this is a third single line comment
 """
 This is for a block level comment
-Block level means multiple lines 
-So with this type of comment i can write a paragraph on multiple lines
 Block level means multiple lines
 So with this type of comment, I can write a paragraph on multiple lines
 
@@ -17,78 +15,12 @@ It is very common in python to use block level comments for documentation purpos
 Usually this is done when writing a function or a class in python
 It can also be used to document a whole script
 """
-# Below we use single quotes for another block level comment
 # below we use single quotes for another block level comment
 '''
 This is another block level comment
 But this one is using a single quote
 Note that you must be consistent with the type of quotes you are using
 '''
-############## OPERATORS ##############
-"""
-# OPERATORS
-operators are simply signs that tell the computer what to do
-There are few categories
-1. Mathematical operators
-These are simly the signs we use in mathematics
-Examples include:
- " + " :for addition
- " - " :for subtraction
- " / " :for division
- " * " :for multiplication
- " % " called modulo. It is used to get the remainder of a division operation
- " // " called integer division. Used only to get the whole number part of a division
- " ** " :for raising a number to a power
-
-2. Logical operators
-These are used for comparison
-Examples include:
-" > " : greater than
-" < " : less than
-" = " : checking for equality
-"!=" : checking non equality
-"""
-print('addition', 2+4) #this is addition
-print('subtraction',4-4) #this is subtraction
-print('division',4/2) #this is division
-print('multiplication',3*2) #this is multiplication
-print('modulo division',6%4) #this will give only the remainder of the division
-print('integer division',6//4) #this will give only the whole number part of the division
-print('raise to a power',2**3) #this is raising a number to a power
-print('modulo division',4%4) 
-
-print(2>4) #this is for greater than
-print(2<4) #this is for less than
-print(2==4) #checking for equality
-print(2!=4) #checking non equality 
-
-"""
-#DATA TYPES
-In python data types tell the computer the type of data you are working with
-Primitive data types: these are data types that form the core basics of python
-   Examples:
-     Integersg - all whole digits both negative and positive
-     (8,-9) 
-     Floats - all decimal digits
-     (2.3, 7.9)
-     Strings - all alpha- numerics. These must always be written inside quotes. Could either be single or double.
-     ('y0988uuii') ('888999'). Strings are basically anything you put inside a quote.
-     Booleans - simply True or False
-     None - simply does not exist
-
-Data structures :These are ways of using the primitives as groups
-   Example:
-      List
-      Tuple
-      Set
-      Dictionary     
-"""
-
-#git fetch upstream master
-#git checkout origin/master
-#git merge upstream/mastergi
-
-
 ###################### OPERATORS ###############################
 """
 # OPERATORS
--- variables.py
+++ variables.py
@@ -1,20 +1,3 @@
-# Examples of varaibles
-name = "Emem"
-print("The value of the var name is", name)
-age = 120
-gender = "female"
-parent = "father" #object oriented programming - class
-old = True
-name = "Itoro"
-print("The value of the var name is",name)
-
-first_number = 50
-next_number = 26
-result = first_number + next_number
-print(result)
-
-
-# we can check the type of value stored in a variable
 """
 # VARIABLES
 ----------------------------------------------------------------
@@ -56,13 +39,6 @@ print(type(name)) # this will check the data type of the value stored in the var
 print(type(next_number)) # this will check the data type of the value stored in the variable called next_number
 print(type(old)) # this will check the data type of the value stored in the variable called old
 
-<<<<<<< HEAD
-print() #the bracket is there because it is a function
-type() #the bracket is there because it is a function
-name # there is no bracket because it is just a variable
-
-=======
 print() # the () is there because it is a function
 type() # the () is there because it is a function
 name # there is no () because it is just a variable
->>>>>>> origin/master
