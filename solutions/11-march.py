05507e9c0705f92eb6093b32539441b36935d3e2
Goodnews <goodnewsrx@gmail.com>
2024-03-17 17:32:09 +0100

    my assignment

---
+++ solutions/11-march.py
@@ -0,0 +1,25 @@
+username=('i love python')
+print(len(username))
+if(len(username)) <3:
+    print('invalid username')
+elif(len(username)) > 15:
+    print('invalid username')
+else:
+    print('valid username') 
+
+          #Question 2
+days_of_the_week=('7') 
+if(days_of_the_week % 2) ==0:
+    print('Even number')
+else:
+    print('Odd number') 
+
+ 
+    #Question 3 
+happy= 8
+if(happy) > 6:
+    print('Hello World!!!')
+elif(happy) < 6:
+    print('Hi World!!!')    
+
+   
