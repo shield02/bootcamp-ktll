134f02be3944f231bc03fcb2c5f60c7762da05b7
Itoro Eti-mfon <etimfonitoro@gmail.com>
2024-03-17 13:42:24 +0100

    boolean conditional assignment

---
+++ solutions/11-march.py
@@ -0,0 +1,25 @@
+username = 'Aitee'
+length_of_username = len(username)
+print(length_of_username)
+
+if 5 < 3:
+    print("Invalid username. must be atleast 3 characters long")
+elif 5 > 15: 
+    print("Invalid username. must be atmost 15 characters long")
+else:
+    print("Valid username")    
+
+schools = 6
+if 6 % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
+
+happy = 13
+if 13 > 6:
+    print("Hello World!!!")
+else:
+    print("Hi World!!!")
+
+
+
