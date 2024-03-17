6ee1203668bc825fe992ba485fc8371a2877fb83
Oba <okunadetewogbade@gmail.com>
2024-03-17 11:50:13 -0700

    solutions/11-march.py

---
+++ solutions/test-assignment.py
@@ -0,0 +1,29 @@
+
+# Question 1
+-------------------------------------------------------
+Create a variable for the string 'Python is Fun!' and
+write the commands that will return the following
+- 'ytho'
+- 'Pytho'
+- 'ython is Fun!'
+- 'Python is Fun!'
+- 'yhni u'
+- 'yoiF'
+- 'Pto sFn'
+"""
+
+"""
+# Question 2
+-------------------------------------------------------
+Create 3 variables to store 
+- 'hello'
+- ','
+- 'world'
+and a fourth variable that will be a concatenation of 
+the first and third variable.
+Write a command that will give the following output
+- 'ello'
+- 'elloworl'
+- 'elwr'
+- 'dlrowolleh'
+"""
