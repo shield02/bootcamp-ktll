3afbbc7d73b02a3c699944114db112fc43d7d771
Uduak <uetimfon@gmail.com>
2024-03-27 07:38:40 +0100

    more loop practice

--- sanbox/class_practice.py
+++ sanbox/class_practice.py
@@ -12,12 +12,13 @@ while number <= 60:
     number = number + 1
 
 # Doing a for loop that prints out multiples of 3 from 0 to 60
-for number in range(0,61):
+for number in range(0, 61):
     if number % 3 == 0:
         print(number)
 
 '''
-# Create a while loop and a for loop that prints out the odd numbers between 0 and 40
+# Create a while loop and a for loop that prints out the odd numbers 
+between 0 and 40
 '''
 # WHILE LOOP
 number = 0
@@ -30,3 +31,8 @@ while number <= 40:
 for o in range(40):
     if o % 2 != 0:
         print(o)
+
+
+
+
+ 
