fe66de7746121dbaae334e28733a6c528bc00963
otuekongenang <myshield02@gmail.com>
2024-03-31 21:30:29 +0100

    additional examples on for loop and while loop

--- looping.py
+++ looping.py
@@ -75,6 +75,8 @@ print("You are a retireeeee now")
 """
 Write a while loop that prints only even numbers between 1 and 20. Tip: use the modulo operator
 """
+# solution is done down down
+
 
 # For loop
 """"
@@ -97,3 +99,67 @@ for number in range(10):    # range(10) will give a sequence of numbers 0,1,2,3,
 for i in range(21):
     if i % 2 == 0:
         print(i)
+        
+# 31-03-2024
+"""
+A for loop is used when we know before hand how long we want the loop to run.
+"""
+# Example: If I want to loop for 10 times
+for number in range(10): # 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+    print("Time - ", number)
+
+print("---------------------------------------------------")
+
+for number in range(1, 11): # 1 is the start value and 11 - 1 will be the stop value: 1,2,3,4,5,6,7,8,9,10
+    print("Time - ", number)
+
+print("---------------------------------------------------")
+
+# Only even numbers between 0 and 20
+for i in range(0, 20, 2): # the last value is the step value, so it will count by 2 numbers:
+    if i == 0:
+        continue # this means jump to the next loop
+    print("Even number - ", i)
+
+for x in range(0, 20, 2):
+    if x == 16:
+        break # this means stop the loop, don't go any futher, don't loop again
+    print("Even numbers up to 16 - ", x)
+
+# something interesting
+word = 'programming'
+vowels = ['a', 'e', 'i', 'o', 'u'] # is a `list` of letters. A list is written with []
+
+for letter in word:
+    # check if the letter is in vowels
+    if letter in vowels:
+        print("I am a vowels,", letter)
+
+value = 2
+for x in range(1, 25):
+    # print("Value raised to the power of x is", value**x) # just the usual way
+    # print(f"Value raised to the power of x is {value**x}") # this is called the f-string pattern
+    print("Value raised to the power of x is {}".format(value**x)) # using the format method
+
+for x in range(1, 25):
+    print(value, "raised to the power of", x ,"is", value**x) # just the usual way
+    # print(f"{value} raised to the power of {x} is {value**x}") # this is called the f-string pattern
+    # print("{} raised to the power of {} is {}".format(value, x, value**x)) # using the format method
+
+
+# solution for a classwork up up
+counter = 1
+while counter < 20:
+    if counter % 2 == 0:
+        print(counter)
+    counter += 1
+    
+# More examples on WHILE loop
+"""Ask a user for a value and the user's favourite word, then cheer the word by the number of times given."""
+fav_number = int(input("What is your favourite number? ")) # input function always collect string values, so we can type cast it to integer
+fav_word = input("What is your favourite word? ")
+counter = 0
+while counter < fav_number:
+    print(fav_word)
+    counter += 1
+
