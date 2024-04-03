756a06753cf6fef65aa32ae398a1ca05400926e1
Uduak <uetimfon@gmail.com>
2024-04-03 20:32:08 +0100

    practice

--- sanbox/rough_work.py
+++ sanbox/rough_work.py
@@ -272,6 +272,21 @@ while number % 2 == 0:
     number = number // 2
     times += 1
     print(times)
+
+'''
+Print out the prime numbers between 100 and 200
+Using while loop
+Using for loop
+'''
+# Using for loop
+for n in range(100,201):
+    if n > 1:
+        for i in range(2,n):
+            if n % i == 0:
+                break
+        else:
+            print(n)
+
   
 
 
