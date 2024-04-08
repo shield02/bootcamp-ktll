cb423163a83b2147a3d1ce9f7716615d544638cd
Edylynn <azure1521@gmail.com>
2024-04-08 17:47:13 +0100

    modifying code in practice file

---
+++ sanbox/practice.py
@@ -0,0 +1,64 @@
+"""Create a variable for the string 'Python is Fun!' and
+write the commands that will return the following
+- 'ytho'
+- 'Pytho'
+- 'ython is Fun!'
+- 'Python is Fun!'
+- 'yhni u'
+- 'yoiF'
+- 'Pto sFn'
+"""
+
+
+#solutions
+my_variable = "python is fun!"
+
+
+#commands that will return the following;
+
+print("ytho")
+print("Pytho")
+print("ython is Fun")
+print("Pytho is Fun")
+print("yhni u")
+print("yoiF")
+print("Pto sFny")
+
+"""
+# Question 2
+-------------------------------------------------------
+Create 3 variables to store 
+- 'hello'
+- ','
+- 'world'
+and a fourth variable that will be a concatenation of 
+the first and third variable.
+Write a command that will give the following output
+- 'ello'
+- 'elloworl'
+- 'elwr'
+- 'dlrowolleh'
+"""
+
+#solution = creating three variables to store
+word_one = "hello"
+punctuation = ","
+word_two = "world"
+
+#concatenation of the first and third variable
+result = word_one + word_two
+print(result)
+
+##commands that will return the following;
+print("ello")
+print("elloworl")
+print("elwr")
+print("dlrowolleh")
+
+
+
+
+
+
+
+
