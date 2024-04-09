b165b5e07c9843ada58c1341821634e240ab5d5b
Uduak <uetimfon@gmail.com>
2024-04-09 10:45:47 +0100

    list_practice

--- sanbox/list_trial.py
+++ sanbox/list_trial.py
@@ -108,3 +108,122 @@ words = ['halleluyah', 'amen', 'victory', 'power', 'glory']
 print(words)
 words.index('victory')
 print(words.index('victory'))
+
+# Adding multiple items to a list. 'Use .extend'
+vegetables = ['carrot', 'peas', 'cabbage', 'lettuce', 'pepper']
+print(vegetables)
+vegetables.extend(['onion', 'egg_plant'])
+print(vegetables)
+
+health_professions = ['nursing', 'optometry', 'radiography', 'dentistry']
+print(health_professions)
+health_professions.extend(['pharmacy', 'micro_biology'])
+print(health_professions)
+
+# Removing an item in a specific position. 'Use .pop()'
+states = ['Abia', 'Ondo', 'Lagos', 'Oyo', 'Edo', 'Imo']
+print(states)
+states.pop(2)
+print(states)
+
+countries = ['England', 'Iraq', 'Chad', 'Niger', 'Japan', 'Qatar']
+print(countries)
+countries.pop(4)
+print(countries)
+
+# To remove all items in a list. 'Use .clear()
+national_defence = ['army', 'navy', 'police', 'marines', 'air_force', 'sss']
+print(national_defence)
+national_defence.clear()
+print(national_defence)
+
+tastes = ['sweet', 'bitter', 'spicy', 'salty', 'bland', 'sour']
+print(tastes)
+tastes.clear()
+print(tastes)
+
+"""
+Question #2
+----------------------------------------------------------------------
+Using the list ['Alice', 'was', 'not', 'a', 'bit', 'hurt'], construct the
+following lists:
+(a) ['not', 'a', 'bit', 'hurt']
+(b) ['Alice', 'was', 'hurt']
+(c) ['Alice', 'hurt', 'a', 'bit']
+(d) ['a', 'bit', 'hurt', 'Alice', 'not']
+"""
+phrases = ['Alice', 'was', 'not', 'a', 'bit', 'hurt']
+order = [2, 3, 4, 5]
+phrases = [phrases[i] for i in order]
+print(phrases)
+
+phrases = ['Alice', 'was', 'not', 'a', 'bit', 'hurt']
+order = [0, 1, 5]
+phrases = [phrases[i] for i in order]
+print(phrases)
+
+phrases = ['Alice', 'was', 'not', 'a', 'bit', 'hurt']
+order = [0, 5, 3, 4]
+phrases = [phrases[i] for i in order]
+print(phrases)
+
+phrases = ['Alice', 'was', 'not', 'a', 'bit', 'hurt']
+order = [3, 4, 5, 0, 2]
+phrases = [phrases[i] for i in order]
+print(phrases)
+
+
+"""
+Question #3
+------------------------------------------------------------------------
+Use only pop and append functions to turn the list ['many', 'a', 'strange', 'tale']
+into ['many', 'a', 'tale']
+"""
+words = ['many', 'a', 'strange', 'tale']
+words.pop(2)
+print(words)
+
+"""
+Question #4
+------------------------------------------------------------------------
+Using list comprehension, create a list of only even numbers from 1 to 100
+with 100 inclusive.
+"""
+numbers = range(1,101)
+even_numbers = [num for num in numbers if num % 2 == 0]
+print(even_numbers)
+
+"""
+# Question #5
+-------------------------------------------------------------------------
+Using list comprehension, create a list of only even numbers raised to the
+second power.
+"""
+# Solution. 'Using even numbers from 0 to 200'
+numbers = range(0,200,2)
+squared_number = [num * num for num in numbers]
+print(squared_number)
+
+"""
+Question #6
+-------------------------------------------------------------------------
+Using list comprehension, create a list of only vowel letters from the list
+of all the letters A-Z.
+"""
+
+import string
+alphabets = string.ascii_uppercase
+print(alphabets)
+vowels = ['A', 'E', 'I', 'O', 'U']
+V = [alph for alph in alphabets if alph in vowels]
+print(V)
+
+
+
+
+
+
+
+
+
+
