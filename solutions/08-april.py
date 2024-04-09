988a82ed488f18f98a9d1568c53f8c49e7577e05
Uduak <uetimfon@gmail.com>
2024-04-09 10:44:58 +0100

    list_assignment

---
+++ solutions/08-april.py
@@ -0,0 +1,193 @@
+"""
+Question #1
+----------------------------------------------------------------------
+Research 10 methods used with list object operations and show at least
+two examples of each of the methods.
+"""
+# Adding to a list. 'Use .append'
+favourite_colours = ['blue', 'lilac', 'peach', 'champagne_gold']
+favourite_colours.append('emerald_green')
+print(favourite_colours)
+
+female_names = ['Uduak', 'Janet', 'Mary', 'Priscilla', 'Mirabel']
+female_names.append('Glory')
+print(female_names)
+
+# Inserting an item at a given position. 'Use .insert'
+feeling = ['sad', 'happy', 'angry', 'restless', 'paranoid']
+feeling.insert(3, 'excited')
+print(feeling)
+
+books_of_the_bible = ['Genesis', 'Revelations', 'Psalms', 'Colossians', 'Malachi']
+books_of_the_bible.insert(0, 'Matthew')
+print(books_of_the_bible)
+
+# Removing an item from the list. 'Use .remove'
+foot_wears = ['sneakers', 'slippers', 'sandals', 'high_heel', 'cover_shoes']
+print(foot_wears)
+foot_wears.remove('sneakers')
+print(foot_wears)
+
+phone_brands = ['samsung', 'itel', 'infinix', 'gionee', 'oppo', 'redmi']
+print(phone_brands)
+phone_brands.remove('gionee')
+print(phone_brands)
+
+# Returns the number of times an element appears in a list. 'Use .count'
+social_media = ['facebook', 'instagram', 'whatsapp', 'linked_in', 'tik_tok']
+print(social_media)
+social_media = ['facebook', 'instagram', 'tik_tok', 'whatsapp', 'tik_tok']
+social_media.count('tik_tok')
+print(social_media.count('tik_tok'))
+
+mens_outfit = ['trouser', 'shirt', 'shorts', 'tie', 'polo']
+print(mens_outfit)
+mens_outfit = ['trouser', 'shirt', 'polo', 'shorts', 'tie', 'polo']
+print(mens_outfit.count('polo'))
+
+# Sort the items of the list in place. 'Use .sort'
+domestic_animals = ['goat', 'dog', 'cat', 'cow', 'donkey', 'chicken']
+print(domestic_animals)
+domestic_animals.sort(key=None, reverse=False)
+print(domestic_animals)
+
+languages = ['french', 'english', 'portuguese', 'german', 'spanish']
+print(languages)
+languages.sort(key=None, reverse=False)
+print(languages)
+
+# Reverse the elements of a list in place. 'Use .reverse'
+classes_of_food = ['proteins', 'carbohydrates', 'vitamins', 'fats_and_oil']
+print(classes_of_food)
+classes_of_food.reverse()
+print(classes_of_food)
+
+body_parts = ['head', 'shoulders', 'knees', 'toes', 'hands', 'legs']
+print(body_parts)
+body_parts.reverse()
+print(body_parts)
+
+# Search the lists and find elements. 'Use .index'
+body_systems = ['digestive', 'reproductive', 'respiratory', 'excretory']
+print(body_systems)
+body_systems.index('excretory')
+print(body_systems.index('excretory'))
+
+words = ['halleluyah', 'amen', 'victory', 'power', 'glory']
+print(words)
+words.index('victory')
+print(words.index('victory'))
+
+# Adding multiple items to a list. 'Use .extend'
+vegetables = ['carrot', 'peas', 'cabbage', 'lettuce', 'pepper']
+print(vegetables)
+vegetables.extend(['onion', 'egg_plant'])
+print(vegetables)
+
+health_professions = ['nursing', 'optometry', 'radiography', 'dentistry']
+print(health_professions)
+health_professions.extend(['pharmacy', 'micro_biology'])
+print(health_professions)
+
+# Removing an item in a specific position. 'Use .pop()'
+states = ['Abia', 'Ondo', 'Lagos', 'Oyo', 'Edo', 'Imo']
+print(states)
+states.pop(2)
+print(states)
+
+countries = ['England', 'Iraq', 'Chad', 'Niger', 'Japan', 'Qatar']
+print(countries)
+countries.pop(4)
+print(countries)
+
+# To remove all items in a list. 'Use .clear()
+national_defence = ['army', 'navy', 'police', 'marines', 'air_force', 'sss']
+print(national_defence)
+national_defence.clear()
+print(national_defence)
+
+tastes = ['sweet', 'bitter', 'spicy', 'salty', 'bland', 'sour']
+print(tastes)
+tastes.clear()
+print(tastes)
+
+"""
+Question #2
+----------------------------------------------------------------------
+Using the list ['Alice', 'was', 'not', 'a', 'bit', 'hurt'], construct the
+following lists:
+(a) ['not', 'a', 'bit', 'hurt']
+(b) ['Alice', 'was', 'hurt']
+(c) ['Alice', 'hurt', 'a', 'bit']
+(d) ['a', 'bit', 'hurt', 'Alice', 'not']
+"""
+# Solution(a)
+phrases = ['Alice', 'was', 'not', 'a', 'bit', 'hurt']
+order = [2, 3, 4, 5]
+phrases = [phrases[i] for i in order]
+print(phrases)
+
+# Solution(b)
+phrases = ['Alice', 'was', 'not', 'a', 'bit', 'hurt']
+order = [0, 1, 5]
+phrases = [phrases[i] for i in order]
+print(phrases)
+
+# Solution(c)
+phrases = ['Alice', 'was', 'not', 'a', 'bit', 'hurt']
+order = [0, 5, 3, 4]
+phrases = [phrases[i] for i in order]
+print(phrases)
+
+# Solution(d)
+phrases = ['Alice', 'was', 'not', 'a', 'bit', 'hurt']
+order = [3, 4, 5, 0, 2]
+phrases = [phrases[i] for i in order]
+print(phrases)
+
+"""
+Question #3
+------------------------------------------------------------------------
+Use only pop and append functions to turn the list ['many', 'a', 'strange', 'tale']
+into ['many', 'a', 'tale']
+"""
+# Solution(3)
+words = ['many', 'a', 'strange', 'tale']
+words.pop(2)
+print(words)
+
+"""
+Question #4
+------------------------------------------------------------------------
+Using list comprehension, create a list of only even numbers from 1 to 100
+with 100 inclusive.
+"""
+# Solution(4)
+numbers = range(1, 101)
+even_numbers = [num for num in numbers if num % 2 == 0]
+print(even_numbers)
+
+"""
+# Question #5
+-------------------------------------------------------------------------
+Using list comprehension, create a list of only even numbers raised to the
+second power.
+"""
+# Solution. 'Using even numbers from 0 to 200'
+numbers = range(0,200,2)
+squared_number = [num * num for num in numbers]
+print(squared_number)
+
+"""
+Question #6
+-------------------------------------------------------------------------
+Using list comprehension, create a list of only vowel letters from the list
+of all the letters A-Z.
+"""
+import string
+alphabets = string.ascii_uppercase
+print(alphabets)
+vowels = ['A', 'E', 'I', 'O', 'U']
+V = [alph for alph in alphabets if alph in vowels]
+print(V)
+
