f2ef03e77a87b2093000856defc4cd25ebbcf46a
Uduak <uetimfon@gmail.com>
2024-04-14 22:04:50 +0100

    function practice

---
+++ sanbox/function_classwork.py
@@ -0,0 +1,29 @@
+def print_full_name(full_name):
+    print(full_name)
+
+
+my_full_name = 'Uduak Edikan Eti-mfon'
+print_full_name(my_full_name)
+
+
+def print_full_name_with_return(full_name):
+    return full_name
+
+
+result = print_full_name
+
+
+def print_fll_name(first_name, last_name):
+    print(first_name + last_name)
+
+
+my_first_name ='Uduak'
+my_last_name = 'Eti-mfon'
+my_full_name = my_first_name + ' ' + my_last_name
+print_fll_name(my_first_name, my_last_name)
+
+
+first_nom = input('Enter your first name: ')
+last_nom = input('Enter your last name: ')
+
+def input('Enter your first name: ')
