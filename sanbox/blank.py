1b3bffe2128cc65604ce24581e7473a6f2143d4d
Uduak <uetimfon@gmail.com>
2024-04-16 13:30:05 +0100

    practice

---
+++ sanbox/blank.py
@@ -0,0 +1,27 @@
+def lucky_number(name):
+    word = 'Hello'
+    sentence = word + ' ' + name + ','
+    phrase = 'Your lucky number is '  
+    s = str(number)
+    message = phrase + s
+    print(sentence, message)
+
+
+name = 'Keme'
+number = len(name) * 7
+lucky_number(name)
+
+n = str(5*4)
+print(n)
+
+
+def month_days(name_month, number_days):
+    print(name_month, number_days)
+
+
+the_name_month = 'June has'
+the_number_days = '30 days'
+month_days(the_name_month, the_number_days)
+
+
+
