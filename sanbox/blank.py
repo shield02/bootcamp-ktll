b5105632a2fecdc79148e7a96ace2b5adb5dfbb2
Uduak <uetimfon@gmail.com>
2024-04-16 17:38:34 +0100

    functions practice

--- sanbox/blank.py
+++ sanbox/blank.py
@@ -1,27 +1,53 @@
+"""
+Question #3
+------------------------------------------------------------------
+The principle of code reuse achived with functions. Functions helps
+in reducing code repeatation.
+------------------------------------------------------------------
+# Convert the following code into a function called `lucky_number`.
+# The function will take only one argument, `name`.
+name = "Keme"
+number = len(name) * 7
+print("Hello " + name + ". Your lucky number is " + str(number))
+"""
+
+
 def lucky_number(name):
-    word = 'Hello'
-    sentence = word + ' ' + name + ','
-    phrase = 'Your lucky number is '  
-    s = str(number)
-    message = phrase + s
-    print(sentence, message)
+    name = 'keme'
+    number = len(name) * 7
+    print(f'Hello {name}. Your lucky number is {number}')
 
 
 name = 'Keme'
-number = len(name) * 7
 lucky_number(name)
-
-n = str(5*4)
-print(n)
 
 
 def month_days(name_month, number_days):
-    print(name_month, number_days)
+    name_month = 'June'
+    number_days = '30'
+    print(f'{name_month} has {number_days} days')
 
 
-the_name_month = 'June has'
-the_number_days = '30 days'
-month_days(the_name_month, the_number_days)
+name_month = 'June'
+number_days = '30'
+month_days(name_month, number_days)
+
+
+def print_full_name():
+    first = input('Enter your first name: ')
+    last = input('Enter your last name: ')
+    print(last,  first)
+
+
+print_full_name()
+
+
+
+
+
+
+
+
 
 
 
