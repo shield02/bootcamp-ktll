85c56f8c5189ca29c5c378ffa05f4f7633edca79
otuekongenang <myshield02@gmail.com>
2024-04-24 07:18:55 +0100

    assigment on tuples and set

---
+++ assignments/23-april.py
@@ -0,0 +1,77 @@
+# INSTRUCTION
+# ---------------------------------------------------------------
+# Remember you are to do your solutions in the solutions folder
+# Create a file with the exact same file name
+# You can copy the questions from here down there
+# But don't type anything inside this file
+# NOTE: Make sure you call all your functions with at least two
+        # examples to show how your function works.
+
+"""
+# Question #1
+------------------------------------------------------------------
+Research 10 methods used with tuples in python and show at least
+two examples of each of the methods.
+"""
+
+"""
+# Question #2
+------------------------------------------------------------------
+Research 10 methods used with sets in python show at least
+two examples of each of the methods.
+"""
+
+"""
+# Question #3
+------------------------------------------------------------------
+Write a function that takes a list of numbers and returns only the
+uniques numbers in the list.
+"""
+
+"""
+# Question #4
+------------------------------------------------------------------
+Return the value 20 from the tuple below:
+tuple1 = ("Orange", [10, 20, 30], (5, 15, 25))
+
+N/B: you do not need to write a function for this question
+"""
+
+"""
+# Question #5
+------------------------------------------------------------------
+Unpacking of values in a tuple.
+If a tuple has 4 values, it can be unpacked by declearing four
+variables and assigning the tuple to it.
+
+Unpack the values of the tuple into 4 variables and print them out.
+tuple2 = (3, 10, 45, 78)
+"""
+
+"""
+# Question #6
+------------------------------------------------------------------
+Create a tuple that has 4 tuples in it. Each of the tuples in the
+main tuple will have a name and age as it's items.
+"""
+
+"""
+# Question #7
+------------------------------------------------------------------
+Write a function that takes two sets and returns a new set of identical
+elements from the two sets.
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+
+Your function will return {30, 40, 50}
+N/B: a set is unordered and immutable so your order might be different.
+"""
+
+"""
+# Question #8
+------------------------------------------------------------------
+Write a function called `calculation` that takes any number integers
+values as positional arguments and returns a tuple of the addition
+of the values and subtraction of the values.
+If only one value is given return a tuple of just that value.
+"""
