e83005a00fd422bb2e9b2d80e8372b686b56d008
Uduak <uetimfon@gmail.com>
2024-04-24 15:00:34 +0100

    practice lst

---
+++ sanbox/lst_practice.py
@@ -0,0 +1,17 @@
+colours = ['read', 'yellow', 'blue', 'green', 'ash']
+print(colours)
+colours[0] = 'red'
+print(colours)
+
+
+def take_list(colours):
+    return set(colours)
+
+
+colours = ['pink', 'purple', 'gold', 'pink', 'silver', 'purple', 'gold']
+
+
+print(take_list(colours))
+
+
+
