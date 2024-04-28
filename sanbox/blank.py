02d483e8118ce1468f3bbd7a2b16d21d0aa49cc1
Uduak <uetimfon@gmail.com>
2024-04-28 23:15:02 +0100

    practice

---
+++ sanbox/blank.py
@@ -0,0 +1,9 @@
+'Create a dictionary of at least three flowers with at least 2 features of each flower '
+flowers = {'hibiscus': ('red', 'sweet'), 'izora': ('pink', 'harsh_fragrance'), 
+           'lily': ('white', 'mild_scent')}
+
+
+acct_dets = {'0294': {'name': 'Ud', 'bal': 100, 'last_deposit': '24th April'},
+             '6366': {'name': 'Aitee', 'bal': 200, 'last_deposit': '5th Feb'},
+               '2599': {'name': 'Emy', 'bal': 500, 'last_deposit': '1st Jan'}}
+
