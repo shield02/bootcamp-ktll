a1f88f220f73c5c7d400436c7f3650fe83ae1278
Edylynn <azure1521@gmail.com>
2024-05-12 16:33:14 +0100

    practice

---
+++ solutions/11-march.py
@@ -0,0 +1,10 @@
+"""
+# Question 1
+------------------------------------------------------------------
+Create a variable called `username`.
+Using a python function check the length of the string in the variable
+and use the result as follows:
+- a case that the length is smaller than 3, print "Invalid username. Must be at least 3 characters long"
+- a case that the lenght is greater than 15, print "Invalid username. Must be at most 15 characters long"
+- a case where the two conditons are not meet, print "Valid username"
+"""
