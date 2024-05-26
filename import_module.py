7317f6a13f22f300682ba944f0d38913f3af2697
otuekongenang <myshield02@gmail.com>
2024-05-26 22:15:14 +0100

    more on importing module

--- import_module.py
+++ import_module.py
@@ -2,7 +2,18 @@ color = 'red'
 food = 'rice'
 
 def cap_color():
-    return 'red'.capitalize()
+    print('red'.capitalize())
 
 def greetings(name):
     return "Good night", name
+
+print(__name__, type(__name__))
+cap_color()
+# you can run your file by passing it to an interpreter - "__main__"
+# import your file as a module - "modulename" and in this case "import_module"
+def main():
+    greetings('uoe')
+    cap_color()
+
+if __name__ == "__main__":
+    main()
