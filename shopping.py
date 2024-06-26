5e98556249efba2cbf6cce9face39fbf9176a29e
otuekongenang <myshield02@gmail.com>
2024-06-26 21:24:41 +0100

    a shopping cart implementation

--- shopping.py
+++ shopping.py
@@ -1,18 +1,24 @@
 shopping_list = []
 print("Welcome Itoro's shopping mall")
 while True:
-    choice = input("Enter 0-to-exit; 1-to add item; 2-to remove item; 3-to view items in your cart ")
+    choice = int(input("Enter 0-to-exit; 1-to add item; 2-to remove item; 3-to view items in your cart "))
     
-    if int(choice) == 0:
+    if choice == 0:
         break
 
-    if int(choice) == 1:
+    if choice == 1:
         print("You can only add 5 items.")
+        cart = {}
         for i in range(1,6):
             item = input("Enter an item: ")
-            shopping_list.append(item)
+            cart[f"item{i}"] = item
             print(f"You have {i} item in the cart")
-    elif int(choice) == 2:
+        shopping_list.append(cart)
+        
+        print(f"Here are the items in your cart: {shopping_list}")
+        # checkout = int(input("Do you want to checkout now? Press 5"))
+
+    elif choice == 2:
         print(f"Items in your cart: {shopping_list}")
         del_choice = input("Enter 0 to empty the cart or an item you want to remove: ")
         if del_choice in shopping_list:
@@ -22,8 +28,12 @@ while True:
         elif int(del_choice) == 0:
             shopping_list.clear()
             print("Your cart is empty.")
-    elif int(choice) == 3:
+    elif choice == 3:
         for item in shopping_list:
             print(item)
 
+
+"""
+DRY - Don't Repeat Yourself
+"""
 
