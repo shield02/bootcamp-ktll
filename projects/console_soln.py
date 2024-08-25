42464b2cdb907c61a8be5457225709a80ef9b71d
otuekongenang <myshield02@gmail.com>
2024-08-25 20:14:35 +0100

    wrote update method and updated created

--- projects/console_soln.py
+++ projects/console_soln.py
@@ -5,7 +5,8 @@ the running the entire application
 """
 import cmd
 from shlex import split
-from . import storage
+import models
+from models.base_model_soln import BaseModel
 
 
 class AirBNBCommand(cmd.Cmd):
@@ -15,7 +16,7 @@ class AirBNBCommand(cmd.Cmd):
     """
     intro = "Welcome to the AirBNB command program"
     prompt = "(airbnb) >>> "
-    __classes = {"BaseModel",}
+    __classes = {"BaseModel": BaseModel}
 
     def do_quit(self, line):
         return True
@@ -34,9 +35,24 @@ class AirBNBCommand(cmd.Cmd):
         elif args[0] not in self.__classes:
             print("** class doesn't exist **")
         else:
-            obj = eval(args[0])()
-            obj.save()
-            print(obj.id)
+            # kwargs = {}
+            # for i in range(1, len(args)):
+            #     key, value = tuple(args[i].split("="))
+            #     if value[0] == '"':
+            #         value = value.strip('"').replace("_", " ")
+            #     else:
+            #         try:
+            #             value = eval(value)
+            #         except (SyntaxError, NameError):
+            #             continue
+            #     kwargs[key] = value
+            # obj = eval(args[0])(**kwargs)
+            # obj.save()
+            # print(obj.id)
+            new_instance = AirBNBCommand.__classes[args[0]]()
+            models.storage.save()
+            print(new_instance)
+            # models.storage.save()
     
     def do_show(self, line):
         """Prints the string representation of an instance."""
@@ -49,7 +65,7 @@ class AirBNBCommand(cmd.Cmd):
             print("** instance id missing **")
         else:
             key = args[0] + "." + args[1]
-            objs = storage.all()
+            objs = models.storage.all()
             if key in objs:
                 print(key)
             else:
@@ -66,7 +82,7 @@ class AirBNBCommand(cmd.Cmd):
             print("** instance id missing **")
         else:
             key = args[0] + "." + args[1]
-            objs = storage.all()
+            objs = models.storage.all()
             if key in objs:
                 del objs[key]
             else:
@@ -79,14 +95,36 @@ class AirBNBCommand(cmd.Cmd):
             if args[0] not in self.__classes:
                 raise NameError()
             
-            obj = storage.all(eval(args[0]))
+            obj = models.storage.all(eval(args[0]))
             for key in obj:
                 print(obj[key].__str__())
         except NameError:
             print("** class doesn't exist **")
 
     def do_update(self, line):
-        pass
+        """Updates an instance based on class name and id"""
+        args = line.split()
+        if not args:
+            print("** class name missing **")
+        elif args[0] not in self.__classes:
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        elif len(args) < 3:
+            print("** attribute name missing **")
+        elif len(args) < 4:
+            print("** value missing **")
+        else:
+            key = args[0] + "." + args[1]
+            objs = models.storage.all()
+            if key in objs:
+                obj = objs[key]
+                atrr_name = args[2]
+                attr_value = args[3]
+                setattr(obj, atrr_name, attr_value)
+                obj.save()
+            else:
+                print("** no instance found **")
 
 if __name__ == "__main__":
     AirBNBCommand().cmdloop()
