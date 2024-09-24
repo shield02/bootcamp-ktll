f554f1cd82d8d7bcc8df254561cf44dee395c9a4
Otuekong <myshield02@gmail.com>
2024-09-24 21:43:35 +0100

    implementation for get total price function

--- test_project/shopping_cart.py
+++ test_project/shopping_cart.py
@@ -7,12 +7,25 @@
 # 2. anyname_that_you_like_test.py
 # The directory is usually called "test"
 
+# FIXTURES
+# Fixtures are used to initialize test functions. They
+# provide a fixed baseline so that tests execute reliably
+# and produce consistent , repeatedable results. They are
+# accessed by test functions as arguement.
+
+# A fixture is typically a normal Python function.
+# To create a fixture, we use a decorator @pytest.fixture
+# annotation. Then we use that function as an arguement in
+# our test.
+
+# MOCKING DEPENDENCIES - Read this up
+
 class ShoppingCart:
-    def __init__(self, max_size=5):
+    def __init__(self, max_size: int = 5) -> None:
         self.items = []
         self.max_size = max_size
 
-    def add(self, item):
+    def add(self, item: str) -> None:
         if self.size() == self.max_size:
             raise OverflowError(f"Cannot add more than {self.max_size}")
         self.items.append(item)
@@ -26,5 +39,8 @@ class ShoppingCart:
     def get_items(self):
         return self.items
 
-    def get_total_price(self):
-        pass
+    def get_total_price(self, price_map: dict[str, int]) -> int:
+        total_price: int = 0
+        for item in self.items:
+            total_price += price_map.get(item)
+        return total_price
