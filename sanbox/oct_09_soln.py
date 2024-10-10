056af1e41c5d6f9315dc55df10c0c198af02f874
Edylynn <azure1521@gmail.com>
2024-10-10 22:53:38 +0100

    assignment one

---
+++ sanbox/oct_09_soln.py
@@ -0,0 +1,31 @@
+"""ASSIGNMENT FOR OCT 9
+---------------------------
+1. Create a variable and store your first name as the value
+2. Create another variable and store your last name as the value
+3. Create another variable and store your favorite number
+4. What is the data type of int * int
+5. What is the result and data type of "int" + "string"
+"""
+
+#Solution: 1
+first_name = "Azure"
+print(first_name)
+
+#2
+last_name = "Blue"
+print(last_name)
+
+#3
+favourite_number = 10
+print(favourite_number)
+
+#4
+number = 23 * 5
+print(number)
+print(type(number))
+
+#5 - concantenation
+data = ("56" + "blue")
+print(data)
+print(type(data))
+
