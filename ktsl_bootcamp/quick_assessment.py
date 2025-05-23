3c76e3c26e03944e1bc795a7277c495826d796f7
otuekong <myshield02@gmail.com>
2025-05-23 21:26:16 +0100

    first solution for the assignment

--- ktsl_bootcamp/quick_assessment.py
+++ ktsl_bootcamp/quick_assessment.py
@@ -1,7 +1,7 @@
 """
 Write a program that allows a teacher to enter exam marks.
-Valid marks a decimal numbers in the range of 0-100. The program 
-    should terminate when the teacher emters '999' and display the 
+Valid marks are decimal numbers in the range of 0-100. The program 
+    should terminate when the teacher enters '999' and display the 
     average mark for the class, rounded to one decimal place.
 
 Additional Rules To Handle:
@@ -13,4 +13,23 @@ Additional Rules To Handle:
     If '999' is entered as before the first valid input, display a 
         message saying 'No results entered'.
 """
-input("Enter marks: ")
+# input("Enter marks: ")
+total_score = 0
+counter = 1
+while True:
+    try:
+        score = int(input("Enter mark(0-100) 999 to quit: "))
+    except ValueError:
+        print("Not a number")
+        continue
+
+    if score >= 0 and score <= 100:
+        total_score += score
+        counter += 1
+    else:
+        counter -= 1
+        print (f"Number outside the range 0-100 inclusive")
+        break
+
+average_score = total_score/counter
+print(f"Average marks: {average_score}")
